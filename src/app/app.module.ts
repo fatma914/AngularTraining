5cd5f2caf37bf316aca0241567accdc771dcbdb0
fatmabenghorbel123 <fatmabenghorbel123@gmail.com>
2021-07-05 23:55:00 +0100

    add contact component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSliderModule} from "@angular/material/slider";
+import { ContactComponent } from './component/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent
   ],
     imports: [
         BrowserModule,
